a144eb98847eaf7d51d48bb481cfa2ad9446574f
Josh Montoya <josh@montoya.io>
2021-11-22 16:47:40 -0800

    implement log

---
+++ log.go
@@ -0,0 +1,28 @@
+package logs
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Log struct {
+	logCore
+
+	Message string `json:"message"`
+	Level   Level  `json:"level"`
+	Tags    Tags   `json:"ddtags"`
+}
+
+func (l *Log) String() string {
+	var (
+		bs  []byte
+		err error
+	)
+
+	if bs, err = json.Marshal(l); err != nil {
+		fmt.Printf("error marshaling datadog log: %v\n", err)
+		return ""
+	}
+
+	return string(bs)
+}
